d2401e3873ccb541feeeadd9bb367c9084e27bc6
piotr.krason <krasonp@gmail.com>
2019-04-10 13:49:50 +0200

    Przechwytywanie daty dokumentu

--- js/script.js
+++ js/script.js
@@ -24,6 +24,7 @@ function leadingZero(i) {
 const currentDate = new Date();
 var dataNow = data.textContent = `${leadingZero(currentDate.getDate())}.${leadingZero(currentDate.getMonth()+1)}.${currentDate.getFullYear()}`;
 var dataTomorrow = `${leadingZero(currentDate.getDate()+1)}.${leadingZero(currentDate.getMonth()+1)}.${currentDate.getFullYear()}`;
+var specialDataFormat = `${currentDate.getFullYear()}-${leadingZero(currentDate.getMonth()+1)}-${leadingZero(currentDate.getDate())}`;
 
 data.value = `Stalowa Wola, ${dataNow}`;
 
@@ -38,3 +39,24 @@ var buttonDrukowanie = document.querySelector('#drukowanie');
 buttonDrukowanie.addEventListener('click', function(){
     window.print();
 })
+
+
+var dataDokumentu = document.querySelector('#data-dokumentu');
+
+// dataDokumentu.value = `${specialDataFormat}`;
+var dataDotted = '';
+
+function dateDottedFormat(){
+    var rok = dataDokumentu.value.substr(0,4),
+    miesiac = dataDokumentu.value.substr(5,2),
+    dzien = dataDokumentu.value.substr(8,2);
+
+    var dataDotted = `${dzien}.${miesiac}.${rok}`;
+
+    data.value = `Stalowa Wola, ${dataDotted}`;
+}
+
+dataDokumentu.addEventListener('input', dateDottedFormat);
+
+
+document.querySelector('#dzien-urlopu').value =`${specialDataFormat}`;
