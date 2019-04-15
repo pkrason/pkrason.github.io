18f026228b4d4753beec88924637b0783980434f
piotr.krason <krasonp@gmail.com>
2019-04-15 15:27:51 +0200

    Termin

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-document.querySelector
+// Options elements variables
+var documentDateOptions = document.querySelector('#data-dokumentu'),
+    nameSurnameOptions = document.querySelector('#imie-nazwisko'),
+    vacationDayOptions = document.querySelector('#vacation-day-options'),
+    vacationTimeOptions = document.querySelector('#vacation-time'),
+    vacationFromToOptions = document.querySelector('#vacation-from-to'),
+    vacationTermFromOptions = document.querySelector('#vacation-term-from-options'),
+    vacationTermToOptions = document.querySelector('#vacation-term-to-options'),
+    nameSurnameEmployee = document.querySelector('#imie-nazwisko-pracownika'),
+    vacationDay = document.querySelector('#vacation-day'),
+    buttonPrint = document.querySelector('#drukowanie');
 
-let input = document.querySelector('.selfadapt');
-let root  = document.documentElement.style;
+// View elements variables
+var vacationTermFrom = document.querySelector('#vacation-term-from'),
+    vacationTermTo = document.querySelector('#vacation-term-to');
 
-/* on input event auto resize the field */
-// input.addEventListener('input', function() {
-//    root.setProperty('--size', this.value.length );
-// });
-
-/* resize the field if it is pre-populated */
-// document.addEventListener("DOMContentLoaded", function() {
-//    root.setProperty('--size', input.value.length);
-// });
 
 var data = document.querySelector('#aktualna-data');
 
@@ -23,38 +25,37 @@ function leadingZero(i) {
 
 }
 
+// Dates
 const currentDate = new Date();
 var dataNow = data.textContent = `${leadingZero(currentDate.getDate())}.${leadingZero(currentDate.getMonth()+1)}.${currentDate.getFullYear()}`;
 var dataTomorrow = `${leadingZero(currentDate.getDate()+1)}.${leadingZero(currentDate.getMonth()+1)}.${currentDate.getFullYear()}`;
 var specialDataFormat = `${currentDate.getFullYear()}-${leadingZero(currentDate.getMonth()+1)}-${leadingZero(currentDate.getDate())}`;
 var specialDataFormatTomorrow = `${currentDate.getFullYear()}-${leadingZero(currentDate.getMonth()+1)}-${leadingZero(currentDate.getDate()+1)}`;
+var specialDataFormatAfterTomorrow = `${currentDate.getFullYear()}-${leadingZero(currentDate.getMonth()+1)}-${leadingZero(currentDate.getDate()+3)}`;
 
+// Setting value of date on view
 data.value = `Stalowa Wola, ${dataNow}`;
 
+// zbadaj coto start
 var polaDaty = document.getElementsByClassName('pole-data');
 for(var i = 0; i < polaDaty.length; i++)
 {
     polaDaty[i].placeholder= `${dataTomorrow}`;
 }
+// zbadaj coto end
 
-var buttonDrukowanie = document.querySelector('#drukowanie');
-
-buttonDrukowanie.addEventListener('click', function(){
+// Print action
+buttonPrint.addEventListener('click', function(){
     window.print();
 })
 
 
-
-
-var documentDateOptions = document.querySelector('#data-dokumentu');
-var nameSurnameOptions = document.querySelector('#imie-nazwisko');
-var vacationDayOptions = document.querySelector('#vacation-day-options');
-var nameSurnameEmployee = document.querySelector('#imie-nazwisko-pracownika');
-var vacationDay = document.querySelector('#vacation-day');
-
-vacationDay.textContent= `${dataTomorrow}`;
-
+// Setting dates
+vacationDay.textContent = `${dataTomorrow}`;
 documentDateOptions.value = `${specialDataFormat}`;
+vacationTermFromOptions.value = `${specialDataFormatTomorrow}`;
+vacationTermToOptions.value = `${specialDataFormatAfterTomorrow}`;
+
 
 
 function fillDocumentDate(){
@@ -81,6 +82,30 @@ function fillVacationDay(){
     vacationDay.textContent = `${dataVacationDotted}`;
 }
 
+
+function fillVacationDayTerm(){
+    var vacationTermFromDotted = vacationTermFromOptions.value;
+    var vacationTermToDotted = vacationTermToOptions.value;
+    // Changing fotmat yyyy-mm-dd to dd.mm.yyyy
+    var rok = vacationTermFromDotted.substr(0,4),
+    miesiac = vacationTermFromDotted.substr(5,2),
+    dzien = vacationTermFromDotted.substr(8,2);``
+    vacationTermFromDotted = `${dzien}.${miesiac}.${rok}`;
+
+    // Filling term from date on view
+    vacationTermFrom.textContent = `${vacationTermFromDotted}`;
+
+    var rok = vacationTermToDotted.substr(0,4),
+    miesiac = vacationTermToDotted.substr(5,2),
+    dzien = vacationTermToDotted.substr(8,2);``
+    vacationTermToDotted = `${dzien}.${miesiac}.${rok}`;
+
+    // Filling term to date on view
+    vacationTermTo.textContent = `${vacationTermToDotted}`;
+}
+
+
+
 function fillNameSurname(){
     // Filling name and surname on view
     nameSurnameEmployee.value = nameSurnameOptions.value;
@@ -91,23 +116,33 @@ documentDateOptions.addEventListener('input', fillDocumentDate);
 nameSurnameOptions.addEventListener('input', fillNameSurname);
 vacationDayOptions.addEventListener('input', fillVacationDay);
 
+vacationTermFromOptions.addEventListener('input', fillVacationDayTerm);
+vacationTermToOptions.addEventListener('input', fillVacationDayTerm);
+
+
 vacationDayOptions.value =`${specialDataFormatTomorrow}`;
 
 
-// 
+// Radio varaibles
 var radioVacationTerm = document.getElementById("radio-term"),
     radioVacationDay = document.getElementById("radio-day"),
     vacationTermContainer = document.querySelector('#vacation-term-container'),
     vacationDayContainer = document.querySelector('#vacation-day-container');
 
 radioVacationTerm.addEventListener('change', function(){
-    vacationTermContainer.classList.remove('invisibility');
-    vacationDayContainer.classList.add('invisibility');
+    vacationTermContainer.classList.remove('invisibility', 'noprint');
+    vacationDayContainer.classList.add('invisibility', 'noprint');
+ 
+    vacationTimeOptions.classList.add('invisibility', 'noprint');
+    vacationFromToOptions.classList.remove('invisibility', 'noprint');
 });
 
 radioVacationDay.addEventListener('change', function(){
-    vacationTermContainer.classList.add('invisibility');
-    vacationDayContainer.classList.remove('invisibility');
+    vacationTermContainer.classList.add('invisibility', 'noprint');
+    vacationDayContainer.classList.remove('invisibility', 'noprint');
+
+    vacationTimeOptions.classList.remove('invisibility', 'noprint');
+    vacationFromToOptions.classList.add('invisibility', 'noprint');
 });
 
 
